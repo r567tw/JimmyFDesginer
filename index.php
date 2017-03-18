be7c6b9a3f68d96a6317e61feb84f88a2c2ef9ff
r567tw <r567tw@gmail.com>
2017-03-18 23:52:01 +0800

    調整版面 去除sass

--- index.php
+++ index.php
@@ -3,13 +3,13 @@
 	<div class="col-md-8">
 	<div class="panel-group">
 		<?php while ( have_posts() ) : the_post(); ?>
-			<div class="panel panel">
+			<div class="panel panel-default">
 				<div class="panel panel-heading">
 					<h1><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
 				</div>
 				<div class="panel-body">
-					<span class="label label-default"><span class="glyphicon glyphicon-time">&nbsp<?php the_time('Y-m-d');?></span></span>&nbsp
-					<span class="label label-default"><span class="glyphicon glyphicon-user"></span>&nbsp<span><?php the_author(); ?></span></span>&nbsp
+					<span class="label label-default"><span class="glyphicon glyphicon-time"><?php the_time('Y-m-d');?></span></span>
+					<!--<span class="label label-default"><span class="glyphicon glyphicon-user"></span>&nbsp<span><?php the_author(); ?></span></span>&nbsp-->
 				</div>
 				<div class="panel-body">	
 					<?php the_excerpt(); ?>
--- single.php
+++ single.php
@@ -10,31 +10,34 @@
 		<?php }; ?>
 			<div class="panel panel-default">
 				<div class="panel panel-heading">
-					<h1><strong><?php the_title(); ?></strong></h1>
-				</div>
-			<div class="panel-body">
-				<div class="row">
-					<div class="col-md-12">
-						<span class="label label-default hidden-print"><span class="glyphicon glyphicon-time"><?php the_time('Y-m-d');?></span></span>
-						<!--<span class="label label-default hidden-print"><span class="glyphicon glyphicon-user"></span><span><?php the_author(); ?></span></span>-->
-					</div>
+					<h1><?php the_title(); ?></h1>
 				</div>
 				<div class="panel-body">
-					<?php the_content(); ?>
-					<div class="panel-footer hidden-print">
-						<?php the_tags('<label>標籤:</lavel> &nbsp','&nbsp&nbsp&nbsp','');?>
+					<div class="row">
+						<div class="col-md-12">
+						<p>
+							<span class="label label-default hidden-print"><span class="glyphicon glyphicon-time"><?php the_time('Y-m-d');?></span></span>
+							<!--<span class="label label-default hidden-print"><span class="glyphicon glyphicon-user"></span><span><?php the_author(); ?></span></span>-->
+						</p>
+						</div>
 					</div>
+					<?php the_content(); ?>
+				</div>
+				<div class="panel-footer hidden-print">
+					<?php the_tags('<label>標籤:</lavel> &nbsp','&nbsp&nbsp&nbsp','');?>
 				</div>
 			</div>
 			<!--存放相關文章-->
 			<div class="panel hidden-print">
-			<?php get_similar_post($post->ID); ?>
+				<?php get_similar_post($post->ID); ?>
 			</div>
+
 			<?php 
 				$history=wp_history_post(get_the_time('Y'), get_the_time('m'), get_the_time('j'));
 				if (!empty($history))
 				{
 			?>
+
 			<div class="panel panel-success hidden-print">
 				<div class="panel-body">
 					<?=$history; ?>
@@ -42,9 +45,7 @@
 			</div>
 			<?php }?>
 			
-			<?php article_author(); ?>
-			
-			
+			<?php article_author(); ?>			
 
 			<div class="col-md-12 hidden-print">
 				<?php comments_template(); ?>
